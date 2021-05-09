8f5dbcf78d24c15e24d33482d4ece7b878bd030d
sEMUer <semuer@gmail.com>
2021-05-09 20:13:09 +0900

    Update vue.config.js

--- project/vue.config.js
+++ project/vue.config.js
@@ -10,13 +10,6 @@ module.exports = {
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+    appleMobileWebAppStatusBarStyle: 'black'
   }
 }
