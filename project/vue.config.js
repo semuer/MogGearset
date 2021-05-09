f6ab5ab276073f23f7577ef35219d2a587bac390
sEMUer <semuer@gmail.com>
2021-05-09 20:22:25 +0900

    Update vue.config.js

--- project/vue.config.js
+++ project/vue.config.js
@@ -10,6 +10,15 @@ module.exports = {
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black'
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      start_url: '/' // The default vue pwa plugin uses '.'. Also many examples on line show '/index.html'. Either of these will
+      // result in a blank screen when installed on iOS or Android, but they will work fine on a PC. The '/' must also be included
+      // in the router. This will meet the requirements of a good start_url and it will install and display correctly on the devices.
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      cleanupOutdatedCaches: true // cleans up cache that is outdated because of a previous version of Workbox.
+    }
   }
 }
