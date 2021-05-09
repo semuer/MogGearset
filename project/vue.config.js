1a09927e9b3789c101aee81d9075b318ed498dac
sEMUer <semuer@gmail.com>
2021-05-09 20:30:28 +0900

    Update configs

--- project/vue.config.js
+++ project/vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     manifestOptions: {
-      start_url: '/' // The default vue pwa plugin uses '.'. Also many examples on line show '/index.html'. Either of these will
+      start_url: '/XIUtilsEquipCalculator/' // The default vue pwa plugin uses '.'. Also many examples on line show '/index.html'. Either of these will
       // result in a blank screen when installed on iOS or Android, but they will work fine on a PC. The '/' must also be included
       // in the router. This will meet the requirements of a good start_url and it will install and display correctly on the devices.
     },
