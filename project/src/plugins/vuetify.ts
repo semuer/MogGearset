fd0e04f1a90b5b02db5fc65b8ae54bb5bd91d6a6
sEMUer <semuer@gmail.com>
2021-07-02 22:36:29 +0900

    Update Site Logos

--- project/src/plugins/vuetify.ts
+++ project/src/plugins/vuetify.ts
@@ -1,7 +1,14 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vue from "vue";
+import Vuetify from "vuetify";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#3790e2",
+      },
+    },
+  },
 });
