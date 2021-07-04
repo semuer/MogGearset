52b05fff19bed06e0d3d5591a97484a90b42897f
sEMUer <semuer@gmail.com>
2021-07-04 21:56:43 +0900

    Fix Job Selector Localization

--- project/src/components/JobSelector.vue
+++ project/src/components/JobSelector.vue
@@ -45,30 +45,33 @@ export default class JobSelector extends Vue {
 
   name = "JobSelector";
 
-  jobList = [
-      { abb: "WAR", name: this.$t('ui.job.WAR') },
-      { abb: "MNK", name: this.$t('ui.job.MNK') },
-      { abb: "WHM", name: this.$t('ui.job.WHM') },
-      { abb: "BLM", name: this.$t('ui.job.BLM') },
-      { abb: "RDM", name: this.$t('ui.job.RDM') },
-      { abb: "THF", name: this.$t('ui.job.THF') },
-      { abb: "PLD", name: this.$t('ui.job.PLD') },
-      { abb: "DRK", name: this.$t('ui.job.DRK') },
-      { abb: "BST", name: this.$t('ui.job.BST') },
-      { abb: "BRD", name: this.$t('ui.job.BRD') },
-      { abb: "RNG", name: this.$t('ui.job.RNG') },
-      { abb: "SAM", name: this.$t('ui.job.SAM') },
-      { abb: "NIN", name: this.$t('ui.job.NIN') },
-      { abb: "DRG", name: this.$t('ui.job.DRG') },
-      { abb: "SMN", name: this.$t('ui.job.SMN') },
-      { abb: "BLU", name: this.$t('ui.job.BLU') },
-      { abb: "COR", name: this.$t('ui.job.COR') },
-      { abb: "PUP", name: this.$t('ui.job.PUP') },
-      { abb: "DNC", name: this.$t('ui.job.DNC') },
-      { abb: "SCH", name: this.$t('ui.job.SCH') },
-      { abb: "GEO", name: this.$t('ui.job.GEO') },
-      { abb: "RUN", name: this.$t('ui.job.RUN') },
-    ];
+  get jobList (){
+    return [
+    { abb: "WAR", name: this.$t('ui.job.WAR') },
+    { abb: "MNK", name: this.$t('ui.job.MNK') },
+    { abb: "WHM", name: this.$t('ui.job.WHM') },
+    { abb: "BLM", name: this.$t('ui.job.BLM') },
+    { abb: "RDM", name: this.$t('ui.job.RDM') },
+    { abb: "THF", name: this.$t('ui.job.THF') },
+    { abb: "PLD", name: this.$t('ui.job.PLD') },
+    { abb: "DRK", name: this.$t('ui.job.DRK') },
+    { abb: "BST", name: this.$t('ui.job.BST') },
+    { abb: "BRD", name: this.$t('ui.job.BRD') },
+    { abb: "RNG", name: this.$t('ui.job.RNG') },
+    { abb: "SAM", name: this.$t('ui.job.SAM') },
+    { abb: "NIN", name: this.$t('ui.job.NIN') },
+    { abb: "DRG", name: this.$t('ui.job.DRG') },
+    { abb: "SMN", name: this.$t('ui.job.SMN') },
+    { abb: "BLU", name: this.$t('ui.job.BLU') },
+    { abb: "COR", name: this.$t('ui.job.COR') },
+    { abb: "PUP", name: this.$t('ui.job.PUP') },
+    { abb: "DNC", name: this.$t('ui.job.DNC') },
+    { abb: "SCH", name: this.$t('ui.job.SCH') },
+    { abb: "GEO", name: this.$t('ui.job.GEO') },
+    { abb: "RUN", name: this.$t('ui.job.RUN') }
+    ]
+  }
+
 
 
      public isActiveJob (abb:string) : boolean
